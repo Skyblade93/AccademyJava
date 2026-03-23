60f8ffb0cad27af6f301f6e3382545c6c114b107
Vincenzo Piccolo <102975436+Skyblade93@users.noreply.github.com>
2026-03-23 11:48:41 +0100

    initial file commit
    primmo commit del file

---
+++ AccademiJava/src/main/java/it/corso/AccademiJava/AccademiJavaApplication.java
@@ -0,0 +1,13 @@
+package it.corso.AccademiJava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccademiJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccademiJavaApplication.class, args);
+	}
+
+}
---
+++ AccademiJava/src/test/java/it/corso/AccademiJava/AccademiJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package it.corso.AccademiJava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccademiJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
