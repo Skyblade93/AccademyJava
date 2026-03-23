ce53b0e1803dd9de86327b88224288417ac16133
giuseppe <giuseppe.sodano.1996@gmail.com>
2026-03-23 16:28:47 +0100

    Add
    Parcel

---
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Model/Parcel.java
@@ -0,0 +1,21 @@
+package it.corso.AccademiJava.Model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "Parcel", schema = "Accademi")
+public class Parcel
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private Double weight;
+    private Integer height;
+    private Integer width;
+    private Integer thickness;
+
+}
