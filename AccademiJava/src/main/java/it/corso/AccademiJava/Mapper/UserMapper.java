02199e4eacc05f274c43653a83f652c68202b58c
Vincenzo <Vincenzo_Piccolo@Outlook.it>
2026-03-24 15:10:56 +0100

    Modify : UserMapper.java

--- AccademiJava/src/main/java/it/corso/AccademiJava/Mapper/UserMapper.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Mapper/UserMapper.java
@@ -2,45 +2,18 @@ package it.corso.AccademiJava.Mapper;
 
 import it.corso.AccademiJava.DTO.UserDto;
 import it.corso.AccademiJava.Model.User;
+import org.modelmapper.ModelMapper;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class UserMapper extends AbstractConverter<User, UserDto> {
 
-    public List<UserDto> toDtoList(List<User> users) {
-        List<UserDto> userDtos = new ArrayList<>();
-        users.forEach(user -> userDtos.add(toDTO(user)));
-        return userDtos;
-    }
 
-    public List<User> toEntityList(List<UserDto> userDtos) {
-        List<User> users = new ArrayList<>();
-        userDtos.forEach(userDto -> users.add(toEntity(userDto)));
-        return users;
-    }
+   final private ModelMapper mapper = new ModelMapper();
+
 
     @Override
-    public UserDto toDTO(User user) {
-        UserDto userDto = new UserDto();
-
-        userDto.setId(user.getId());
-        userDto.setName(user.getName());
-        userDto.setDescription(user.getDescription());
-        userDto.getContacts().addAll(user.getContacts());
-
-        return userDto;
-    }
+    public UserDto toDTO(User entity) { return mapper.map(entity, UserDto.class); }
 
     @Override
-    public User toEntity(UserDto userDto) {
-        User user = new User();
-
-        user.setId(userDto.getId());
-        user.setName(userDto.getName());
-        user.setDescription(userDto.getDescription());
-        user.getContacts().addAll(userDto.getContacts());
-
-        return user;
-    }
+    public User toEntity(UserDto dto) { return mapper.map(dto, User.class);}
 }
