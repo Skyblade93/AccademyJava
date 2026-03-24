32e7160c960705cf6c1b5d241fe9775dd3fa1a5a
giova <giovanni14.piccolo03@gmail.com>
2026-03-24 16:22:13 +0100

    Changes to carrelloMapper

--- AccademiJava/src/main/java/it/corso/AccademiJava/Mapper/CarrelloMapper.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Mapper/CarrelloMapper.java
@@ -1,56 +1,8 @@
 package it.corso.AccademiJava.Mapper;
 
 import it.corso.AccademiJava.DTO.CarrelloDto;
-import it.corso.AccademiJava.DTO.UserDto;
 import it.corso.AccademiJava.Model.Carrello;
-import it.corso.AccademiJava.Model.User;
 import org.modelmapper.ModelMapper;
-import org.springframework.beans.factory.annotation.Autowired;
-
-import java.util.ArrayList;
-import java.util.List;
-
-/*
-public class CarrelloMapper {
-
-    @Autowired
-    private UserMapper userMapper;
-
-    public List<CarrelloDto> toDtoList(List<Carrello> carrelli){
-        List<CarrelloDto> carrelliDtos = new ArrayList<>();
-        carrelli.forEach(carrello -> carrelliDtos.add(toDto(carrello)));
-        return carrelliDtos;
-    }
-
-    public List<Carrello> toEnityList(List<CarrelloDto> carrelliDtos){
-        List<Carrello> carrelli = new ArrayList<>();
-        carrelliDtos.forEach(carrelloDto -> carrelli.add(toEntity(carrelloDto)));
-        return carrelli;
-    }
-
-    public CarrelloDto toDto(Carrello carrello) {
-        CarrelloDto carrelloDto = new CarrelloDto();
-
-        carrelloDto.setId(carrello.getId());
-        carrelloDto.setQuantita(carrello.getQuantita());
-        carrelloDto.setPrezzoTotale(carrello.getPrezzoTotale());
-        carrelloDto.setUser(userMapper.toDTO(carrello.getUser()));
-
-        return carrelloDto;
-    }
-
-    public Carrello toEntity(CarrelloDto carrelloDto) {
-        Carrello carrello = new Carrello();
-
-        carrello.setId(carrelloDto.getId());
-        carrello.setQuantita(carrelloDto.getQuantita());
-        carrello.setPrezzoTotale(carrelloDto.getPrezzoTotale());
-        carrello.setUser(userMapper.toEntity(carrelloDto.getUser()));
-
-        return carrello;
-    }
-}
- */
 
 public class CarrelloMapper extends AbstractConverter<Carrello, CarrelloDto> {
 
