43396c12def341877004dfeb5a4a2e3e90fcba82
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-03-24 15:56:26 +0100

    Changes AbstractConverter.java AccademiJavaApplicationTests.class application.properties BeansConfig.java createdFiles.lst createdFiles.lst DroneMapper.java inputFiles.lst inputFiles.lst it.corso.AccademiJava.AccademiJavaApplicationTests.txt pom.xml TEST-it.corso.AccademiJava.AccademiJavaApplicationTests.xml

--- AccademiJava/src/main/java/it/corso/AccademiJava/Mapper/DroneMapper.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Mapper/DroneMapper.java
@@ -13,14 +13,12 @@ public class DroneMapper extends AbstractConverter<Drone, DroneDto> {
     // Trasforma Entity -> DTO in modo automatico
     @Override
     public DroneDto toDTO (Drone entity) {
-        // Questa riga fa tutto il lavoro dei setter che avevi prima!
         return modelMapper.map(entity, DroneDto.class);
     }
 
     // Trasforma DTO -> Entity in modo automatico
     @Override
     public Drone toEntity(DroneDto dto) {
-        // Anche qui, ModelMapper accoppia i campi da solo
         return modelMapper.map(dto, Drone.class);
     }
 
