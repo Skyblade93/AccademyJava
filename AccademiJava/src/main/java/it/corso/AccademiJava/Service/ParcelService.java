a1a18679989bf66db2c4c2f53b624f4db407d172
giuseppe <giuseppe.sodano.1996@gmail.com>
2026-03-25 16:02:15 +0100

    ParcelService.java

--- AccademiJava/src/main/java/it/corso/AccademiJava/Service/ParcelService.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Service/ParcelService.java
@@ -3,22 +3,35 @@ package it.corso.AccademiJava.Service;
 import it.corso.AccademiJava.DTO.AutoDto;
 import it.corso.AccademiJava.DTO.ParcelDto;
 import it.corso.AccademiJava.Mapper.ParcelMapper;
+import it.corso.AccademiJava.Mapper.ProductMapper;
 import it.corso.AccademiJava.Model.Parcel;
 import it.corso.AccademiJava.Repository.ParcelRepository;
+import it.corso.AccademiJava.Repository.ProductRepository;
+import it.corso.AccademiJava.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class ParcelService extends AbstractService<Parcel, ParcelDto>{
+public class ParcelService extends AbstractService<Parcel, ParcelDto> {
 
     @Autowired
     ParcelMapper parcelMapper;
     @Autowired
     ParcelRepository parcelRepository;
 
-        public ParcelDto findByWeight(Double weight) {return parcelMapper.toDTO(parcelRepository.findByWeight(weight));
+    public ParcelService(ParcelRepository parcelRepository,
+                         ParcelMapper parcelMapper) {
+
+        super(parcelRepository, parcelMapper);
+        this.parcelRepository = parcelRepository;
+        this.parcelMapper = parcelMapper;
+    }
 
 
+    public ParcelDto findByWeight(Double weight) {
+        return parcelMapper.toDTO(parcelRepository.findByWeight(weight));
     }
 
 }
+
+
