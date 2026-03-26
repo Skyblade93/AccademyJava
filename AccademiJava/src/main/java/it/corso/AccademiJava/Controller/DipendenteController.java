2639934c29f309711735aa410f07980aeb155ff7
giacopa97 <giacopa97@gmail.com>
2026-03-26 09:03:20 +0100

    Modify DipendenteController

--- AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DipendenteController.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DipendenteController.java
@@ -5,6 +5,8 @@ import it.corso.AccademiJava.Service.DipendenteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("Dipendente")
 @CrossOrigin(origins="http://localhost:4200")
@@ -13,10 +15,34 @@ public class DipendenteController extends AbstractController<DipendenteDto> {
     @Autowired
     private DipendenteService dipendenteService;
 
-    @GetMapping("/findByNome")
-    public DipendenteDto findByNome(@RequestParam("nomeDipendente") String nomeDipendente) {
-        return dipendenteService.findByNome(nomeDipendente);
+    @GetMapping("/findByNomeDipendenteAndCognomeDipendenteAndNumeroTelefono")
+    public DipendenteDto findByNomeDipendenteAndCognomeDipendenteAndNumeroTelefono(
+            @RequestParam("nomeDipendente") String nomeDipendente,
+            @RequestParam("cognomeDipendente") String cognomeDipendente,
+            @RequestParam("numeroTelefono") Integer numeroTelefono) {
+        return dipendenteService.findByNomeDipendenteAndCognomeDipendenteAndNumeroTelefono(nomeDipendente, cognomeDipendente, numeroTelefono);
     }
 
+    @GetMapping("/findByNomeDipendenteAndCognomeDipendente")
+    public DipendenteDto findByNomeDipendenteAndCognomeDipendenteAndNumeroTelefono(
+            @RequestParam("nomeDipendente") String nomeDipendente,
+            @RequestParam("cognomeDipendente") String cognomeDipendente) {
+        return dipendenteService.findByNomeAndCognome(nomeDipendente, cognomeDipendente);
+    }
+
+    @GetMapping("/findByEta")
+    public List<DipendenteDto> findByEta(@RequestParam("eta") Integer eta) {
+        return dipendenteService.findByEta(eta);
+    }
+
+    @GetMapping("/findByEtaGreaterThan")
+    public List<DipendenteDto> findByEtaGreaterThan(@RequestParam("eta") Integer eta) {
+        return dipendenteService.findByEtaGreaterThan(eta);
+    }
+
+    @GetMapping("/findByEmail")
+    public DipendenteDto findByEmail(@RequestParam("email") String email) {
+        return dipendenteService.findByEmail(email);
+    }
 
 }
