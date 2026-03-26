347706c7f59095f04eed3399da207938ea5def63
giacopa97 <giacopa97@gmail.com>
2026-03-26 12:58:07 +0100

    Changes application.properties Dipendente.java DipendenteController.java DipendenteDto.java DipendenteRepository.java DipendenteService.java

--- AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DipendenteController.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DipendenteController.java
@@ -51,7 +51,7 @@ public class DipendenteController extends AbstractController<DipendenteDto> {
         return dipendenteService.findByEmailAndEta(email, eta);
     }
 
-    @GetMapping("/findByNomeDipendenteAndEta")
+    @GetMapping("/findByNomeAndEta")
     public DipendenteDto findByNomeDipendenteAndEta(@RequestParam("nomeDipendente") String nomeDipendente,
                                                     @RequestParam("eta") Integer eta) {
         return dipendenteService.findByNomeDipendenteAndEta(nomeDipendente, eta);
--- AccademiJava/src/main/java/it/corso/AccademiJava/Repository/DipendenteRepository.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Repository/DipendenteRepository.java
@@ -38,10 +38,10 @@ public interface DipendenteRepository extends JpaRepository<Dipendente,Integer> 
 
     // Metodo 3 creca per nome dipendente
     // 🔹 QUERY NATIVE (come metodo 3)
-    @Query(value = "SELECT * FROM accademi.dipendente d WHERE d.nome_dipendente = ?1", nativeQuery = true)
+    @Query(value = "SELECT * FROM Dipendente d WHERE d.nomeDipendente = ?1", nativeQuery = true)
     Dipendente findByNomeDipendente(String nomeDipendente);
 
     // Metodo 3 cerca per email ed numero di telefono
-    @Query(value = "SELECT * FROM accademi.dipendente d WHERE d.email = ?1 AND d.numero_telefono = ?2", nativeQuery = true)
-    Dipendente findByEmailAndNumeroTelefono(String email, Integer numeroTelefono);
+    @Query(value = "SELECT * FROM Dipendente d WHERE d.email = ?1 AND d.numeroTelefono = ?2", nativeQuery = true)
+    Dipendente findByEmailAndNumber(String email, Integer numeroTelefono);
 }
