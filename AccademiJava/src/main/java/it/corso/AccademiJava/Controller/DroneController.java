2427716bf2c46e4d626bcd713ddf7859f99ee4df
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-03-26 10:13:32 +0100

    Changes         DroneController.java         DroneMapper.java         DroneService.java

--- AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DroneController.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DroneController.java
@@ -10,31 +10,23 @@ import java.util.List;
 @RestController
 @RequestMapping("Drone")
 @CrossOrigin(origins = "http://localhost:4200")
-// RIMOSSO <DroneDto> dopo il nome della classe
 public class DroneController extends AbstractController<DroneDto> {
 
     @Autowired
     private DroneService service;
 
     @PostMapping("/insert")
-    // RIMOSSO <DroneDto> davanti al tipo di ritorno
     public DroneDto insert(@RequestBody DroneDto dto) {
-        return (DroneDto) service.insert(dto);
+        return service.insert(dto);
     }
 
-    /*
     @GetMapping("/findByModello")
-    // RIMOSSO <DroneDto> davanti al tipo di ritorno
     public DroneDto findByModello(@RequestParam("modello") String modello) {
-        return (DroneDto) service.findByModello(modello);
+        return service.findByModello(modello);
     }
 
     @GetMapping("/trovaTramiteIniziale")
-    // RIMOSSO <DroneDto> davanti alla List
     public List<DroneDto> trovaTramiteIniziale(@RequestParam("find") Character i) {
-        // Qui non serve il cast (DroneDto), il service restituisce già il tipo giusto
         return service.trovaTramiteIniziale(i);
     }
-
-     */
 }
--- AccademiJava/src/main/java/it/corso/AccademiJava/Service/DroneService.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Service/DroneService.java
@@ -1,18 +1,16 @@
 package it.corso.AccademiJava.Service;
 
-import it.corso.AccademiJava.Mapper.Converter;
 import it.corso.AccademiJava.Mapper.DroneMapper;
 import it.corso.AccademiJava.Model.Drone;
 import it.corso.AccademiJava.DTO.DroneDto;
 import it.corso.AccademiJava.Repository.DroneRepository;
 import it.corso.AccademiJava.Repository.UserRepository;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
-public class DroneService<DroneDto> extends AbstractService<Drone, DroneDto> {
+public class DroneService extends AbstractService<Drone, DroneDto> {
 
     private final DroneRepository droneRepository;
     private final UserRepository userRepository;
@@ -20,19 +18,23 @@ public class DroneService<DroneDto> extends AbstractService<Drone, DroneDto> {
     public DroneService(DroneRepository droneRepository,
                         DroneMapper droneMapper,
                         UserRepository userRepository) {
-        // Passiamo repository e mapper al costruttore della classe padre (AbstractService)
-        super((JpaRepository<Drone, Integer>) droneRepository, (Converter<Drone, DroneDto>) droneMapper);
+        // Passiamo repo e mapper alla classe base AbstractService
+        super(droneRepository, droneMapper);
         this.droneRepository = droneRepository;
         this.userRepository = userRepository;
     }
-/*
-    // Metodo che restituisce DTO (quello che serve al Controller)
-    public <DroneDto> DroneDto findByModello(String modello) {
-        // Cerchiamo l'entity tramite il nome del modello
-        Drone entity = droneRepository.findByModelloCustom(modello);
-        // Usiamo il 'converter' ereditato da AbstractService per trasformarlo in DTO
-        return converter.toDto(entity);
-    }
-*/
 
+    // Metodo per cercare un drone per modello
+    public DroneDto findByModello(String modello) {
+        Drone entity = (Drone) droneRepository.findByModello(modello);
+        // Usiamo il cast (DroneMapper) per accedere al metodo toDTO che hai appena scritto
+        return ((DroneMapper) converter).toDTO(entity);
+    }
+
+    // Metodo per cercare i droni tramite l'iniziale
+    public List<DroneDto> trovaTramiteIniziale(Character i) {
+        List<Drone> entities = droneRepository.findByModelloStartingWith(String.valueOf(i));
+        // Usiamo il metodo toDtoList che hai nel tuo Mapper
+        return ((DroneMapper) converter).toDtoList(entities);
+    }
 }
