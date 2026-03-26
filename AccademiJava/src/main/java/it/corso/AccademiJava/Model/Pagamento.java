b50da982640235533f3ae1be1eca817fc5437dc0
AlecCode97-star <alessandro1492@live.it>
2026-03-26 15:01:22 +0100

    modify:        Pagamento.java
    create:PagamentoController.java
           PagamentoRepository.java
           PagamentoService.java

--- AccademiJava/src/main/java/it/corso/AccademiJava/Model/Pagamento.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Model/Pagamento.java
@@ -1,28 +1,29 @@
 package it.corso.AccademiJava.Model;
 
-import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
 
 @Getter
 @Setter
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@Table(name ="pagamento",schema = "Accademi")
+@Table(name="Pagamento",schema = "Accademi")
+
 public class Pagamento {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private Double importo;
     private LocalDateTime dataPagamento;
-
+    @Enumerated(EnumType.STRING)
+    private StatoPagamento stato;
     @Enumerated(EnumType.STRING)
     private MetodoPagamento metodo;
+
+    @OneToOne
+    @JoinColumn(name="user_id")
+    private User user;
 
     @Enumerated(EnumType.STRING)
     private StatoPagamento stato;
