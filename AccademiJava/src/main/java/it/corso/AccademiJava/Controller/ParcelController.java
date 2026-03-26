6db863201a69d63d9cd8b80ea0b27d37419ad926
giuseppe <giuseppe.sodano.1996@gmail.com>
2026-03-26 11:19:57 +0100

    Update: ParcelController.java ParcelRepository.java ParcelService.java

---
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Controller/ParcelController.java
@@ -0,0 +1,30 @@
+package it.corso.AccademiJava.Controller;
+
+import it.corso.AccademiJava.DTO.ParcelDto;
+import it.corso.AccademiJava.Service.ParcelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("Parcel")
+@CrossOrigin(origins = "http://localhost:4200")
+public class ParcelController extends AbstractController<ParcelDto>
+{
+    @Autowired
+    private ParcelService service;
+
+    @GetMapping("/FindByWeight")
+    public ParcelDto FindByWeight(@RequestParam("weight")Double weight)
+    {
+     return service.findByWeight(weight);
+    }
+
+    @GetMapping("/FindByHeight")
+    public ParcelDto FindByHeight(@RequestParam("height")Integer height)
+    {
+        return service.findByHeight(height);
+    }
+
+
+
+}
--- AccademiJava/src/main/java/it/corso/AccademiJava/Repository/ParcelRepository.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Repository/ParcelRepository.java
@@ -1,21 +1,20 @@
 package it.corso.AccademiJava.Repository;
 
-
-import it.corso.AccademiJava.Model.Auto;
 import it.corso.AccademiJava.Model.Parcel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-import java.util.Optional;
 
 @Repository // Entrata DB
 public interface ParcelRepository extends JpaRepository<Parcel,Integer>
 {
     @Query("SELECT p FROM Parcel p WHERE p.weight = :weight")
     Parcel findByWeight(@Param("weight") Double weight);
+
+    @Query("SELECT p FROM Parcel p WHERE p.height = :height")
+    Parcel findByHeight(@Param("height") Integer height);
 
 
 
