15ee1c26ea1ccdcecd7f278e58a0bbb3739c3bb8
Mor33mila <lorenzolomb20@gmail.com>
2026-03-27 17:33:08 +0100

    Add. Auto.java

--- AccademiJava/src/main/java/it/corso/AccademiJava/Model/Auto.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Model/Auto.java
@@ -18,12 +18,13 @@ public class Auto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;  //collegato all'id di user che autoincrementato già
+    private Integer id;  //identificativo auto
 
     private String modello;  //modello dell'auto
 
     private String marca;  //marca dell'auto
 
+    @Column(nullable = false, unique = true)
     private String targa;  //targa dell'auto
 
     @Enumerated(EnumType.STRING) // salva il nome dell'enum nel DB
