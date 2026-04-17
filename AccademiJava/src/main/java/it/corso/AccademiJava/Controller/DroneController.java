d1a5600e2c4af30084cd54ffca1d8fe0a13d5b77
Giovanni Iadelise <iadelisegiovanni2000@gmail.com>
2026-04-17 10:51:49 +0200

    Modify : controller aggiunto GetAll

--- AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DroneController.java
+++ AccademiJava/src/main/java/it/corso/AccademiJava/Controller/DroneController.java
@@ -4,29 +4,38 @@ import it.corso.AccademiJava.DTO.DroneDto;
 import it.corso.AccademiJava.Service.DroneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 @RestController
 @RequestMapping("Drone")
-@CrossOrigin(origins = "http://localhost:4200")
-public class DroneController extends AbstractController<DroneDto> {
+@CrossOrigin(origins = "http://localhost:4200") // Permette ad Angular di entrare
+public class DroneController {
 
     @Autowired
     private DroneService service;
+
+    @GetMapping("/getall")
+    public List<DroneDto> getAll() {
+        return (List<DroneDto>) service.getAll();
+    }
 
     @PostMapping("/insert")
     public DroneDto insert(@RequestBody DroneDto dto) {
         return service.insert(dto);
     }
 
+    @PutMapping("/update")
+    public DroneDto update(@RequestBody DroneDto dto) {
+        return service.update(dto);
+    }
+
+    @DeleteMapping("/delete")
+    public void delete(@RequestParam("id") Integer id) {
+        service.delete(id);
+    }
+
     @GetMapping("/findByModello")
     public DroneDto findByModello(@RequestParam("modello") String modello) {
         return service.findByModello(modello);
-    }
-
-    @GetMapping("/trovaTramiteIniziale")
-    public List<DroneDto> trovaTramiteIniziale(@RequestParam("find") Character i) {
-        return service.trovaTramiteIniziale(i);
     }
 }
